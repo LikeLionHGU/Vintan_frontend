3f55a01e7541bcac96b6b8d6f6d8efbbe32301e3
sungyu0309 <tjsrbyt@gmail.com>
2025-08-13 03:25:08 +0900

    feat : main 페이지 내 영상 무한 재생
    - mp4 영상 페이지 내 무한 재생
    - 오른쪽 하단 깎이는 스타일 적용

---
+++ src/components/Main/VideoSection.js
@@ -0,0 +1,38 @@
+import { Box, styled } from "@mui/material";
+import ServiceVideo from "../../videos/Comp.mp4";
+
+// 1) 둥근 모서리 래퍼 + 대각선 깎기
+const VideoCard = styled(Box)({
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  borderRadius: 20, // 사방 라운드
+  overflow: "hidden", // 모서리 안으로 잘라냄
+  clipPath: "polygon(0 0, 100% 0, 100% 65%, 54% 100%, 0 100%)",
+  background: "black", // 비디오가 없는 부분 배경색
+});
+
+const CutVideo = (props) => (
+  <Box
+    component="video"
+    autoPlay
+    loop
+    muted
+    playsInline
+    preload="metadata"
+    {...props}
+    sx={{
+      width: "100%",
+      objectFit: "cover",
+      display: "block",
+    }}
+  />
+);
+
+export default function VideoSection() {
+  return (
+    <VideoCard>
+      <CutVideo src={ServiceVideo} />
+    </VideoCard>
+  );
+}
