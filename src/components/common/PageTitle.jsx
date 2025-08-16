7bf3aa2ac30b50e8329ab0cb12cd92255f7bc8b5
sungyu0309 <tjsrbyt@gmail.com>
2025-08-16 17:38:00 +0900

    add : page title, text input component
    - 다른 파일에서 사용할 수 있도록 컴포넌트화
    - text input의 경우 props로 value, 상태 관리 함수 전달
    - props에 icon의 유무에 따라 padding 값 조정

---
+++ src/components/common/PageTitle.jsx
@@ -0,0 +1,28 @@
+import { Box, styled, Typography } from "@mui/material";
+import React from "react";
+import TitleWrapper from "../../imgs/report/title-wrapper.svg";
+
+export default function PageTitle({ text }) {
+  return (
+    <Box>
+      <ImgWrapper>
+        <Box component="img" src={TitleWrapper} />
+        <Typography variant="title3" color="#fff">
+          {text}
+        </Typography>
+      </ImgWrapper>
+    </Box>
+  );
+}
+
+const ImgWrapper = styled(Box)`
+  display: inline-block;
+  position: relative;
+  > span {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    white-space: nowrap;
+  }
+`;
---
+++ src/components/common/TextInput.jsx
@@ -0,0 +1,52 @@
+import { Box, Input, styled } from "@mui/material";
+import React from "react";
+import Search from "../../imgs/report/search.png";
+
+export default function TextInput({
+  icon,
+  placeholder,
+  value,
+  onChange,
+  search,
+}) {
+  return (
+    <Box sx={{ position: "relative" }}>
+      <Img component="img" src={icon} />
+      <StyledInput
+        placeholder={placeholder ? placeholder : ""}
+        value={value ?? ""}
+        onChange={onChange}
+        sx={{ paddingLeft: `${icon ? "44px" : "12px"}` }}
+      />
+      {search ? <SearchImg component="img" src={Search} /> : null}
+    </Box>
+  );
+}
+
+const Img = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 12px;
+  transform: translate(0, -50%);
+`;
+
+const StyledInput = styled(Input)(({ theme }) => ({
+  width: "100%",
+  border: `1px solid ${theme.palette.grey[500]}`,
+  borderRadius: 8,
+  padding: "8px 12px",
+  outline: "none",
+  "&:before, &:after": { display: "none" },
+
+  "&:focus-within": {
+    outline: `2px solid ${theme.palette.grey[800]}`,
+    outlineOffset: "-1px",
+  },
+}));
+
+const SearchImg = styled(Box)`
+  position: absolute;
+  top: 50%;
+  right: 12px;
+  transform: translate(0, -50%);
+`;
