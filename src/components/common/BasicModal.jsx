0fd0645915298445a4f0a246ea5d94ce6f8c2fcf
sungyu0309 <tjsrbyt@gmail.com>
2025-08-18 01:56:37 +0900

    add : modal common component
    재사용 가능한 모달 컴포넌트

--- src/components/common/BasicModal.jsx
+++ src/components/common/BasicModal.jsx
@@ -1,29 +1,52 @@
-import * as React from "react";
-import Modal from "@mui/material/Modal";
+// src/components/common/BasicModal.jsx
+import React from "react";
+import { Box, Button, Modal, styled } from "@mui/material";
+import CloseIcon from "../../imgs/report/close.svg";
 
-const style = {
+const modalStyle = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  borderRadius: 2,
+  p: "20px",
+  outline: "none",
+  maxHeight: "80vh",
+  overflowY: "auto",
+  minWidth: 520,
 };
 
-export default function BasicModal({ children, open, handleClose }) {
+export default function BasicModal({
+  open,
+  handleClose,
+  children,
+  keepMounted = true,
+  modalWidth,
+  modalHeight,
+}) {
   return (
-    <>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>{children}</div>
-      </Modal>
-    </>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-desc"
+      keepMounted={keepMounted} // 성능/포커스 유지에 유리
+      disableScrollLock // 필요 시 스크롤 잠금 방지
+    >
+      <Box sx={{ ...modalStyle, width: modalWidth, height: modalHeight }}>
+        <ClosButton onClick={handleClose}>
+          <Box component="img" src={CloseIcon} />
+        </ClosButton>
+        {children}
+      </Box>
+    </Modal>
   );
 }
+
+const ClosButton = styled(Button)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
