fd99eddce0279c0826f2c7458405bb323cb384b3
sungyu0309 <tjsrbyt@gmail.com>
2025-08-25 17:24:51 +0900

    edit : 창업 스퀘어 워딩 수정

--- src/page/Rating.jsx
+++ src/page/Rating.jsx
@@ -54,7 +54,7 @@ export default function Rating() {
         <PageTitle text="질문" isHere={false} link="/community/question" />
       </HorizontalBox>
       <Typography variant="body1">
-        커뮤니티에서 원하는 상권의 평점과 장단점을 확인해보세요. <br />
+        창업스퀘어에서 원하는 상권의 평점과 장단점을 확인해보세요. <br />
         궁금한 점은 질문게시판에서 직접 물어보고 답을 받아보세요.
       </Typography>
       <LocationContainer p={4} my={3}>
